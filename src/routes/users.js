f9ea240110f46b6aa85e08352cf557d368e97de6
Tudor Toma <tudortoma2509@gmail.com>
2022-05-26 17:49:59 +0300

    Minor changes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,5 @@
 import bcrypt from 'bcryptjs/dist/bcrypt';
 import { BadRequestError } from '../utils/errors';
-import bcrypt from 'bcryptjs/dist/bcrypt';
 
 const express = require('express');
 const router = express.Router();
